3434c10bb6b8d33b7b9e6e4d9aa84bcac27f8ee6
Tomona Sako <sakotomona@gmail.com>
2020-03-08 22:43:42 -0700

    finish 682

---
+++ 682.Baseball Game.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+var calPoints = function(ops) {
+    let sum = [];
+    for (let i = 0; i < ops.length; i++){
+        switch(ops[i]){
+            case "C":
+                sum.pop();
+                break;
+            case "D":
+                sum.push(sum[sum.length - 1] * 2);
+                break;
+            case "+":
+                sum.push(sum[sum.length - 1] + sum[sum.length - 2]);
+                break;
+            default:
+                sum.push(Number(ops[i]))
+        }
+        console.log(sum)
+};
+    return sum.reduce((a, b) => (a + b));
+}
+
+
+let answer = calPoints(
+    ["5","-2","4","C","D","9","+","+"]);
+console.log(answer)
